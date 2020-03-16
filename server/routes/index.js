271c76dc434a65a3d30f33ce013e972f0b1222cb
prasetya <prasetya.baskara@btpn.com>
2020-03-16 12:33:08 +0700

    dotenv docs

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const testController = require("../controllers/testController");
 
+// to use .env on server ( /.env )
+console.log('this is from .env:', process.env.intro); // this is from .env: this_is_env_intro
+
 const router = express.Router();
 
 router.get('/api/testGet', testController.testGet);
