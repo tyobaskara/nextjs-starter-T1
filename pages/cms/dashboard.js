d596e05f93d3422e09b14c63a0e899df15d42543
prasetya <prasetya.baskara@btpn.com>
2020-03-17 20:10:35 +0700

    dashboard

--- pages/cms/dashboard.js
+++ pages/cms/dashboard.js
@@ -1,27 +1,13 @@
-import { PureComponent } from 'react';
-import Constants from '../../public/static/js/constants';
-
 // Layout
 import AdminLayout from '../../components/_layouts/admin.layout';
 
 // Container
 import CmsDashboardContainer from '../../containers/cmsDashboard/cmsDashboard.container';
 
-const { 
-  cms: {
-    route: { loginPage },
-    loginTokenName
-  }
-} = Constants;
-
-class DashboardPage extends PureComponent {
-  render() {
-    return (
-      <AdminLayout>
-        <CmsDashboardContainer />
-      </AdminLayout>
-    )
-  }
+export default function DashboardPage() {
+  return (
+    <AdminLayout>
+      <CmsDashboardContainer />
+    </AdminLayout>
+  );
 }
-
-export default DashboardPage;
