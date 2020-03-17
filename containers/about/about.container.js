57bf295bfb94329e7dd9c4e369070b27e508cd9c
prasetya <prasetya.baskara@btpn.com>
2020-03-17 20:27:33 +0700

    aboutcontainer

--- containers/about/about.container.js
+++ containers/about/about.container.js
@@ -1,13 +1,7 @@
-import { PureComponent } from 'react';
-
-class AboutContainer extends PureComponent {
-  render() {
-    return (
-      <div className="container">
-        <h1>About Us</h1>
-      </div>
-    )
-  }
-}
-
-export default AboutContainer;
+export default function AboutContainer () {
+  return (
+    <div className="container">
+      <h1>About Us</h1>
+    </div>
+  )
+};
