ae5ba71979ebcceea603db61caebece0c246a162
prasetya <prasetya.baskara@btpn.com>
2020-04-25 13:42:34 +0700

    herobanner html

--- components/HeroBanner/HeroBanner.component.js
+++ components/HeroBanner/HeroBanner.component.js
@@ -13,51 +13,53 @@ export default function HeroBanner(props) {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    className: 'heroBanner'
+    className: 'heroBanner-slider'
   };
   
   return (
-    <Slider {...settings}>
-    
-      <div className='heroBanner-wrapper'>
-        <img 
-          className='heroBanner-bg' 
-          src='https://dummyimage.com/1920x514/333333/ffffff&text=1920x514'  
-        />
+    <section className='heroBanner'>
+      <Slider {...settings}>
+      
+        <div className='heroBanner-wrapper'>
+          <img 
+            className='heroBanner-bg' 
+            src='https://dummyimage.com/1920x514/333333/ffffff&text=1920x514'  
+          />
 
-        <div className='container'>
-          <div className='row'>
-            <div className='col-sm-8'>
-              <div className='heroBanner__highlight active'>
-                <h1>One Hospital <br/>Management System <br/>For All Hospital Needs</h1>
-                <p>Thought experiments (Gedankenexperimenten) are “facts” in the sense that they have a “real life” correlate in the form of electrochemical activity in the brain.</p>
-                <Link href={navigateTo('about-us', language)}>
-                  <a className='btn-blue'>See More Testimonial</a>
-                </Link>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-sm-8'>
+                <div className='heroBanner__highlight active'>
+                  <h1>One Hospital <br/>Management System <br/>For All Hospital Needs</h1>
+                  <p>Thought experiments (Gedankenexperimenten) are “facts” in the sense that they have a “real life” correlate in the form of electrochemical activity in the brain.</p>
+                  <Link href={navigateTo('about-us', language)}>
+                    <a className='btn-blue'>See More Testimonial</a>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div className='heroBanner-wrapper'>
-        <img 
-          className='heroBanner-bg' 
-          src='https://dummyimage.com/1920x514/333333/ffffff&text=1920x514'  
-        />
+        <div className='heroBanner-wrapper'>
+          <img 
+            className='heroBanner-bg' 
+            src='https://dummyimage.com/1920x514/333333/ffffff&text=1920x514'  
+          />
 
-        <div className='container'>
-          <div className='row'>
-            <div className='col-sm-8'>
-              <div className='heroBanner__highlight'>
-                <h2>One Hospital <br/>Management System <br/>For All Hospital Needs</h2>
-                <p>Thought experiments (Gedankenexperimenten) are “facts” in the sense that they have a “real life” correlate in the form of electrochemical activity in the brain.</p>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-sm-8'>
+                <div className='heroBanner__highlight'>
+                  <h2>One Hospital <br/>Management System <br/>For All Hospital Needs</h2>
+                  <p>Thought experiments (Gedankenexperimenten) are “facts” in the sense that they have a “real life” correlate in the form of electrochemical activity in the brain.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-    </Slider>
+      </Slider>
+    </section>
   );
 }
